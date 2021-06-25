17781d3f0662db9c1fdb514ada910426be072e61
levyhanunes <levyhanunes@gmail.com>
2021-06-24 21:07:00 -0300

    Get API keys from alphavantage.co

---
+++ dca-calculator/service/APIService.swift
@@ -0,0 +1,16 @@
+//
+//  APIService.swift
+//  dca-calculator
+//
+//  Created by user on 24/06/21.
+//
+
+import Foundation
+
+struct APIService {
+    
+    let API_KEY1 = "RWVLSERK4BDBH3G8"
+    let API_KEY2 = "ABZ7RETS6GB69BPB"
+    let API_KEY3 = "KURLNPR9DD7MHVMB"
+    
+}
